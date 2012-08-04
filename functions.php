0f12c9822ca718ff086bf0d296850d841f9bfe51
Matthias Pfefferle <pfefferle@gmail.com>
2012-08-04 23:12:51 +0200

    fixed content_width

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+	$content_width = 600; /* pixels */
 
 if ( ! function_exists( 'sempress_setup' ) ):
 /**
