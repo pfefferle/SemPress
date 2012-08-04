ec40d4de1c2678eae96356865023ce5cf1938105
Matthias Pfefferle <pfefferle@gmail.com>
2012-08-04 23:03:46 +0200

    aside template

--- content-aside.php
+++ content-aside.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<article <?php post_id(); ?> <?php post_class(); ?>>
+<aside <?php post_id(); ?> <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title p-entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 	</header><!-- .entry-header -->
@@ -26,11 +26,31 @@
 	<?php endif; ?>
 
 	<footer class="entry-meta">
-		<?php sempress_posted_on(); ?>
+	  <?php sempress_posted_on(); ?>
+	  <?php
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'sempress' ) );
+			if ( $categories_list && sempress_categorized_blog() ) :
+		?>
+		<span class="cat-links">
+			<?php printf( __( 'in %1$s', 'sempress' ), $categories_list ); ?>
+		</span>
+		<?php endif; // End if categories ?>
+
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', __( ', ', 'sempress' ) );
+			if ( $tags_list ) :
+		?>
+		<span class="tag-links">
+			<?php printf( __( 'tagged with %1$s', 'sempress' ), $tags_list ); ?>
+		</span>
+		<?php endif; // End if $tags_list ?>
+		
 		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
 		<span class="sep"> | </span>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'sempress' ), __( '1 Comment', 'sempress' ), __( '% Comments', 'sempress' ) ); ?></span>
 		<?php endif; ?>
 		<?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 	</footer><!-- #entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+</aside><!-- #post-<?php the_ID(); ?> -->
