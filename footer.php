145ee94d8d30ed1fe449b43bb9bfbd0c49e3e29f
Matthias Pfefferle <pfefferle@gmail.com>
2012-08-07 12:10:46 +0300

    removed whitespaces

--- footer.php
+++ footer.php
@@ -9,16 +9,16 @@
  */
 ?>
 
-	</div><!-- #main -->
+  </div><!-- #main -->
 
-	<footer id="colophon" role="contentinfo">
-		<div id="site-generator">
-			<?php do_action( 'sempress_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'sempress' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'sempress' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'sempress' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s based on the awesome %3$s-Theme by %4$s.', 'sempress' ), 'SemPress', '<a href="http://notizblog.org/" rel="designer">Matthias Pfefferle</a>', 'Toolbox', '<a href="http://automattic.com/" rel="designer">Automattic</a>'); ?>
-		</div>
-	</footer><!-- #colophon -->
+  <footer id="colophon" role="contentinfo">
+    <div id="site-generator">
+      <?php do_action( 'sempress_credits' ); ?>
+      <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'sempress' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'sempress' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'sempress' ), 'WordPress' ); ?></a>
+      <span class="sep"> | </span>
+      <?php printf( __( 'Theme: %1$s by %2$s based on the awesome %3$s-Theme by %4$s.', 'sempress' ), 'SemPress', '<a href="http://notizblog.org/" rel="designer">Matthias Pfefferle</a>', 'Toolbox', '<a href="http://automattic.com/" rel="designer">Automattic</a>'); ?>
+    </div>
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
