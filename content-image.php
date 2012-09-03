b17ce12d175ea02a55291ebf74d3a98fefbfe343
Matthias Pfefferle <pfefferle@gmail.com>
2012-09-03 16:59:29 +0200

    design for content-type "image"

--- content-image.php
+++ content-image.php
@@ -1,30 +1,62 @@
 <?php
 /**
- * The template for displaying posts in the Image Post Format on index and archive pages
- *
- * Learn more: http://codex.wordpress.org/Post_Formats
- *
  * @package SemPress
  * @since SemPress 0.1
  */
 ?>
 
-<article <?php sempress_post_id(); ?> <?php post_class(); ?>>
+<article <?php sempress_post_id(); ?> <?php post_class(); ?> itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
-    <h1 class="entry-title p-entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <h1 class="entry-title p-entry-title" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+
+    <?php if ( 'post' == get_post_type() ) : ?>
+    <div class="entry-meta">
+      <?php sempress_posted_on(); ?>
+    </div><!-- .entry-meta -->
+    <?php endif; ?>
   </header><!-- .entry-header -->
 
-  <div class="entry-content e-entry-content">
+  <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+  <div class="entry-summary e-entry-summary" itemprop="description">
+    <?php the_excerpt(); ?>
+  </div><!-- .entry-summary -->
+  <?php else : ?>
+  <div class="entry-content e-entry-content" itemprop="articleBody">
     <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sempress' ) ); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
+  <?php endif; ?>
 
   <footer class="entry-meta">
-    <?php sempress_posted_on(); ?>
+    <?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+      <?php
+        /* translators: used between list items, there is a space after the comma */
+        $categories_list = get_the_category_list( __( ', ', 'sempress' ) );
+        if ( $categories_list && sempress_categorized_blog() ) :
+      ?>
+      <span class="cat-links">
+        <?php printf( __( 'Posted in %1$s', 'sempress' ), $categories_list ); ?>
+      </span>
+      <span class="sep"> | </span>
+      <?php endif; // End if categories ?>
+
+      <?php
+        /* translators: used between list items, there is a space after the comma */
+        $tags_list = get_the_tag_list( '', __( ', ', 'sempress' ) );
+        if ( $tags_list ) :
+      ?>
+      <span class="tag-links">
+        <?php printf( __( 'Tagged %1$s', 'sempress' ), $tags_list ); ?>
+      </span>
+      <span class="sep"> | </span>
+      <?php endif; // End if $tags_list ?>
+    <?php endif; // End if 'post' == get_post_type() ?>
+
     <?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
-    <span class="sep"> | </span>
     <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'sempress' ), __( '1 Comment', 'sempress' ), __( '% Comments', 'sempress' ) ); ?></span>
+    <span class="sep"> | </span>
     <?php endif; ?>
-    <?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+
+    <?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="edit-link">', '</span>' ); ?>
   </footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
