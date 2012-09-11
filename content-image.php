ec5977bbc66411f9a3a39249297a88513805e6f9
Matthias Pfefferle <pfefferle@gmail.com>
2012-09-11 20:14:19 +0200

    somne changes

--- content-image.php
+++ content-image.php
@@ -5,9 +5,9 @@
  */
 ?>
 
-<article <?php sempress_post_id(); ?> <?php post_class(); ?> itemscope itemtype="http://schema.org/ImageObject">
+<article <?php sempress_post_id(); ?> <?php post_class(); ?>>
   <header class="entry-header">
-    <h1 class="entry-title p-entry-title" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+    <h1 class="entry-title p-entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
     <?php if ( 'post' == get_post_type() ) : ?>
     <div class="entry-meta">
@@ -17,11 +17,11 @@
   </header><!-- .entry-header -->
 
   <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-  <div class="entry-summary e-entry-summary" itemprop="description">
+  <div class="entry-summary e-entry-summary">
     <?php the_excerpt(); ?>
   </div><!-- .entry-summary -->
   <?php else : ?>
-  <div class="entry-content e-entry-content" itemprop="description">
+  <div class="entry-content e-entry-content">
     <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sempress' ) ); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
