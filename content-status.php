fae7766cee1cf1da9ac0ca0395c1606d981c03fa
pfefferle <pfefferle@gmail.com>
2012-09-26 13:56:40 +0200

    changed the markup of the post-type "status"

--- content-status.php
+++ content-status.php
@@ -12,12 +12,6 @@
 <aside <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
     <h1 class="entry-title p-entry-title" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
-
-    <?php if ( 'post' == get_post_type() ) : ?>
-    <div class="entry-meta">
-      <?php sempress_posted_on(); ?>
-    </div><!-- .entry-meta -->
-    <?php endif; ?>
   </header><!-- .entry-header -->
 
   <?php if ( is_search() ) : // Only display Excerpts for search pages ?>
@@ -31,4 +25,8 @@
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
   <?php endif; ?>
+  
+  <footer class="entry-meta">
+    <?php sempress_posted_on(); ?>
+  </footer>
 </aside><!-- #post-<?php the_ID(); ?> -->
