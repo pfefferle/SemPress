f78b15990c14fd19f11532fbad6b9493931a8e1b
pfefferle <pfefferle@gmail.com>
2012-10-10 11:52:05 +0200

    fixed wrong use of "aside"

--- content-aside.php
+++ content-aside.php
@@ -9,7 +9,7 @@
  */
 ?>
 
-<aside <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
+<article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
     <h1 class="entry-title p-entry-title" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
   </header><!-- .entry-header -->
@@ -53,4 +53,4 @@
     <?php endif; ?>
     <?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
   </footer><!-- #entry-meta -->
-</aside><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-<?php the_ID(); ?> -->
