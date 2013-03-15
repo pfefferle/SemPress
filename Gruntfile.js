0de0611e9a61695cc046cc443c4f3496f1a6c9ac
pfefferle <pfefferle@gmail.com>
2013-03-15 10:48:19 +0100

    added grunt-replace

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,34 @@ module.exports = function(grunt) {
           'SemPress/css/editor-style.css': 'sass/editor-style.scss'
         }
       }
+    },
+    replace: {
+      style: {
+        options: {
+          variables: {
+            'author': '<%= pkg.author.name %>',
+            'author_url': '<%= pkg.author.url %>',
+            'version': '<%= pkg.version %>',
+            'license': '<%= pkg.license.name %>',
+            'license_version': '<%= pkg.license.version %>',
+            'license_url': '<%= pkg.license.url %>',
+            'name': '<%= pkg.name %>',
+            'description': '<%= pkg.description %>',
+            'homepage': '<%= pkg.homepage %>'
+          },
+          prefix: '@@'
+        },
+        files: [
+          { expand: true, flatten: true, src: ['SemPress/style.css'], dest: 'SemPress' }
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-replace');
 
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
-
+  grunt.registerTask('default', ['sass', 'replace']);
 };
   
