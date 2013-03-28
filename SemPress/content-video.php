44e2c8604c5854a8880083f3dea8ff2be7d138f2
pfefferle <pfefferle@gmail.com>
2013-03-28 15:38:00 +0100

    updated template with the new post-format features

--- SemPress/content-video.php
+++ SemPress/content-video.php
@@ -7,6 +7,10 @@
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
+		<div class="entry-media">
+			<?php the_audio(); ?>
+		</div>
+
     <h1 class="entry-title p-name" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
 
     <div class="entry-meta">
@@ -20,7 +24,7 @@
   </div><!-- .entry-summary -->
   <?php else : ?>
   <div class="entry-content e-content" itemprop="description articleBody">
-    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sempress' ) ); ?>
+    <?php the_remaining_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sempress' ) ); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
   <?php endif; ?>
