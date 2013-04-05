5b4ad25b53daf18b1b25ebfa454a5415f767bd59
pfefferle <pfefferle@gmail.com>
2013-04-05 11:02:57 +0200

    changed post-formats

--- SemPress/functions.php
+++ SemPress/functions.php
@@ -81,7 +81,7 @@ function sempress_setup() {
   ) );
 
   // Add support for the Aside, Gallery Post Formats...
-  add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery', 'quote', 'link', 'audio', 'video', 'status' ) );
+  add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'status' ) );
   add_theme_support( 'structured-post-formats', array( 'image', 'video', 'audio', 'quote' ) );
 
   /**
