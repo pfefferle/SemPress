d4555361eab2d6fd40fcb6e5a0cbf25e735e696e
pfefferle <pfefferle@gmail.com>
2013-04-09 16:28:51 +0200

    added "chat" post-type

--- SemPress/content-chat.php
+++ SemPress/content-chat.php
@@ -7,12 +7,7 @@
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
-    <?php if ( '' != get_the_post_thumbnail() ) { ?>
-    <div class="entry-media">
-      <?php the_post_thumbnail( "post-thumbnail", array( "itemprop" => "image" ) ); ?>
-		</div>
-    <?php } ?>    
-    <h1 class="entry-title p-name" itemprop="name"><?php the_title(); ?></h1>
+    <h1 class="entry-title p-name" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
 
     <div class="entry-meta">
       <?php sempress_posted_on(); ?>
@@ -20,7 +15,8 @@
   </header><!-- .entry-header -->
 
   <div class="entry-content e-content" itemprop="articleBody description">
-    <?php the_content(); ?>
+    <?php sempress_the_post_thumbnail(); ?>
+    <?php the_post_format_chat(); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
 
