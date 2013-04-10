ace41084ec11e0461c0eee3adb2506ab0046c5c6
pfefferle <pfefferle@gmail.com>
2013-04-10 11:16:10 +0200

    changed the place for the post-thumbnail

--- SemPress/content-page.php
+++ SemPress/content-page.php
@@ -11,9 +11,14 @@
   <header class="entry-header">
     <h1 class="entry-title p-name" itemprop="name"><?php the_title(); ?></h1>
   </header><!-- .entry-header -->
-
+  
+  <?php if ( '' != get_the_post_thumbnail() ) { ?>
+  <div class="entry-media">
+    <?php the_post_thumbnail( "post-thumbnail", array( "itemprop" => "image", "class" => "aligncenter" ) ); ?>
+  </div>
+  <?php } ?>
+  
   <div class="entry-content e-content" itemprop="description">
-    <?php sempress_the_post_thumbnail(); ?>
     <?php the_content(); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
     <?php edit_post_link( __( 'Edit', 'sempress' ), '<span class="edit-link">', '</span>' ); ?>
--- SemPress/content-single.php
+++ SemPress/content-single.php
@@ -4,25 +4,32 @@
  * @since SemPress 1.0.0
  */
 ?>
-
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_blog_itemprop("blogPost"); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <header class="entry-header">
-    <?php if ( '' != get_the_post_thumbnail() ) { ?>
-    <div class="entry-media">
-      <?php the_post_thumbnail( "post-thumbnail", array( "itemprop" => "image" ) ); ?>
-		</div>
-    <?php } ?>    
-    <h1 class="entry-title p-name" itemprop="name"><?php the_title(); ?></h1>
+    <h1 class="entry-title p-name" itemprop="name"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
 
-    <div class="entry-meta">
+    <?php if ( 'post' == get_post_type() ) : ?>
+    <div class="entry-meta">      
       <?php sempress_posted_on(); ?>
     </div><!-- .entry-meta -->
+    <?php endif; ?>
   </header><!-- .entry-header -->
 
-  <div class="entry-content e-content" itemprop="articleBody description">
-    <?php the_content(); ?>
+  <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+  <div class="entry-summary p-summary" itemprop="description">
+    <?php the_excerpt(); ?>
+  </div><!-- .entry-summary -->
+  <?php else : ?>
+  <?php if ( '' != get_the_post_thumbnail() ) { ?>
+  <div class="entry-media">
+    <?php the_post_thumbnail( "post-thumbnail", array( "itemprop" => "image", "class" => "aligncenter" ) ); ?>
+  </div>
+  <?php } ?>
+  <div class="entry-content e-content" itemprop="description articleBody">
+    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sempress' ) ); ?>
     <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'sempress' ), 'after' => '</div>' ) ); ?>
   </div><!-- .entry-content -->
+  <?php endif; ?>
 
   <?php get_template_part( 'entry', 'footer' ); ?>
 </article><!-- #post-<?php the_ID(); ?> -->
