30f974a9e4b9a6a67668ebc42efdd48b90916b44
pfefferle <pfefferle@gmail.com>
2013-04-17 10:05:13 +0200

    added backwards compatibility

---
+++ SemPress/inc/back-compat.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * SemPress back compat functionality.
+ *
+ * @package WordPress
+ * @since SemPress 1.4
+ */
+
+if ( ! function_exists( 'the_post_format_video' ) ) :
+  function the_post_format_video() {
+    return;
+  }
+endif;
+
+if ( ! function_exists( 'the_post_format_audio' ) ) :
+  function the_post_format_audio() {
+    return;
+  }
+endif;
+
+if ( ! function_exists( 'the_post_format_image' ) ) :
+  function the_post_format_image( $attached_size = 'full' ) {
+    return;
+  }
+endif;
+
+if ( ! function_exists( 'the_post_format_chat' ) ) :
+  function the_post_format_chat() {
+    return the_content();
+  }
+endif;
+
+if ( ! function_exists( 'the_remaining_content' ) ) :
+  function the_remaining_content( $more_link_text, $stripteaser ) {
+    return the_content( $more_link_text, $stripteaser );
+  }
+endif;
+
+if ( ! function_exists( 'get_post_format_meta' ) ) :
+  function get_post_format_meta( $ID ) {
+    return;
+  }
+endif;
