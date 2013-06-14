8424ad2e4da8624479725627430bd2cce8c650bb
pfefferle <pfefferle@gmail.com>
2013-06-14 10:54:41 +0200

    fixed compatibility

--- SemPress/inc/back-compat.php
+++ SemPress/inc/back-compat.php
@@ -1,54 +1,5 @@
 <?php
 /**
- * SemPress back compat functionality.
- *
- * @package WordPress
- * @since SemPress 1.4
- */
-
-if ( ! function_exists( 'the_post_format_video' ) ) :
-  function the_post_format_video() {
-    return;
-  }
-endif;
-
-if ( ! function_exists( 'the_post_format_audio' ) ) :
-  function the_post_format_audio() {
-    return;
-  }
-endif;
-
-if ( ! function_exists( 'the_post_format_image' ) ) :
-  function the_post_format_image( $attached_size = 'full' ) {
-    return;
-  }
-endif;
-
-if ( ! function_exists( 'the_post_format_chat' ) ) :
-  function the_post_format_chat() {
-    return the_content();
-  }
-endif;
-
-if ( ! function_exists( 'the_remaining_content' ) ) :
-  function the_remaining_content( $more_link_text = null, $strip_teaser = false ) {
-    return the_content( $more_link_text, $strip_teaser );
-  }
-endif;
-
-if ( ! function_exists( 'get_post_format_meta' ) ) :
-  function get_post_format_meta( $ID ) {
-    return;
-  }
-endif;
-
-if ( ! function_exists( 'the_post_format_quote' ) ) :
-  function the_post_format_quote( &$post = null ) {
-    return;
-  }
-endif;
-
-/**
  * adds the new HTML5 input types to the comment-form
  *
  * @param string $form
