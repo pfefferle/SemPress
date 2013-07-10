a6e4c2bd85e63f2ac53d765adbf37e64dd9240ba
pfefferle <pfefferle@gmail.com>
2013-07-10 15:13:02 +0200

    added post-format template

---
+++ SemPress/taxonomy-post_format.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying Post Format pages.
+ *
+ * Used to display archive-type pages for posts with a post format.
+ * If you'd like to further customize these Post Format views, you may create a
+ * new template file for each specific one.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package SemPress
+ * @since SemPress 1.4.0
+ */
+
+get_header(); ?>
+
+    <section id="primary">
+      <main id="content" role="main">
+
+      <?php if ( have_posts() ) : ?>
+
+        <header class="page-header">
+          <h1 class="page-title"><?php
+            printf( __( '%s Archives', 'sempress' ), '<span>' . get_post_format_string( get_post_format() ) . '</span>' );
+          ?></h1>
+        </header>
+
+        <?php sempress_content_nav( 'nav-above' ); ?>
+
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+          <?php
+            /* Include the Post-Format-specific template for the content.
+             * If you want to overload this in a child theme then include a file
+             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+             */
+            get_template_part( 'content', get_post_format() );
+          ?>
+
+        <?php endwhile; ?>
+
+        <?php sempress_content_nav( 'nav-below' ); ?>
+
+      <?php else : ?>
+
+        <article id="post-0" class="post no-results not-found">
+          <header class="entry-header">
+            <h1 class="entry-title p-entry-title"><?php _e( 'Nothing Found', 'sempress' ); ?></h1>
+          </header><!-- .entry-header -->
+
+          <div class="entry-content e-entry-content">
+            <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'sempress' ); ?></p>
+            <?php get_search_form(); ?>
+          </div><!-- .entry-content -->
+        </article><!-- #post-0 -->
+
+      <?php endif; ?>
+
+      </main><!-- #content -->
+    </section><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
