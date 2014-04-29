a8fc4c6246a50b8ecc7ac4ab2d9ed6bf5302fdf0
Dylan Kinnett <dylan@nocategories.net>
2014-04-28 22:58:24 -0400

    ADDED ENTRY-HEADER.PHP
    there was already an ``entry-footer.php`` file so I added a
    corresponding ``entry-header.php file``.

--- sempress/content-audio.php
+++ sempress/content-audio.php
@@ -6,14 +6,8 @@
 ?>
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_semantics("post"); ?>>
-  <header class="entry-header">
-    <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+  <?php get_template_part( 'entry', 'header' ); ?>
 
-    <div class="entry-meta">
-      <?php sempress_posted_on(); ?>
-    </div><!-- .entry-meta -->
-  </header><!-- .entry-header -->
-  
   <?php if ( is_search() ) : // Only display Excerpts for Search ?>
   <div class="entry-summary p-summary" itemprop="description">
     <?php the_excerpt(); ?>
---
+++ sempress/entry-header.php
@@ -0,0 +1,7 @@
+  <header class="entry-header">
+    <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="Permalink to <?php the_title_attribute(); ?> " rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+
+    <div class="entry-meta">
+      <?php sempress_posted_on(); ?>
+    </div><!-- .entry-meta -->
+  </header><!-- .entry-header -->
