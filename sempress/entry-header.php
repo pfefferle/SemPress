6e68a7da71e71e0aebd7754d769697e03b23be5c
pfefferle <pfefferle@gmail.com>
2014-05-04 20:31:53 +0200

    re-added i18n

--- sempress/entry-header.php
+++ sempress/entry-header.php
@@ -1,5 +1,5 @@
   <header class="entry-header">
-    <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="Permalink to <?php the_title_attribute(); ?> " rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
+    <h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h1>
 
     <div class="entry-meta">
       <?php sempress_posted_on(); ?>
