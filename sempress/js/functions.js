df7b5891eb07dcddd90c4beb35b99f97e8a6b76e
pfefferle <pfefferle@gmail.com>
2015-03-17 22:11:41 +0100

    removed unused functions

--- sempress/js/functions.js
+++ sempress/js/functions.js
@@ -12,22 +12,6 @@
     if ( ! nav ) {
       return;
     }
-
-    button = nav.find( '.menu-toggle' );
-    if ( ! button ) {
-      return;
-    }
-
-    // Hide button if menu is missing or empty.
-    menu = nav.find( '.nav-menu' );
-    if ( ! menu || ! menu.children().length ) {
-      button.hide();
-      return;
-    }
-
-    $( '.menu-toggle' ).on( 'click.twentyfourteen', function() {
-      nav.toggleClass( 'toggled-on' );
-    } );
   } )();
 
   $( function() {
