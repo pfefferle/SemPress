72c5af6d92705a4501917fccbc81e85382421ea5
Matthias Pfefferle <matthias@pfefferle.org>
2016-02-16 21:06:53 +0100

    remove the 'user agent detection'

--- sempress/header.php
+++ sempress/header.php
@@ -8,18 +8,7 @@
  * @since SemPress 1.0.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 6]>
-<html id="ie6" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 7]>
-<html id="ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html id="ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
