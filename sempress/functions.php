4c164d27ca4c1aa621986187d18ef3ce270e5f59
Matthias Pfefferle <matthias@pfefferle.org>
2016-02-20 20:36:37 +0100

    fixed #38

--- sempress/functions.php
+++ sempress/functions.php
@@ -356,22 +356,23 @@ if ( ! function_exists( 'sempress_comment' ) ) :
 	?>
 		<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 			<article id="comment-<?php comment_ID(); ?>" class="comment <?php $comment->comment_type; ?>" itemprop="comment" itemscope itemtype="http://schema.org/Comment">
-				<div class="comment-content p-summary p-name" itemprop="text name description"><?php comment_text(); ?></div>
-					<footer>
-						<div class="comment-meta commentmetadata">
-							<address class="comment-author p-author author vcard hcard h-card" itemprop="creator" itemscope itemtype="http://schema.org/Person">
-							<?php printf( '<cite class="fn p-name" itemprop="name">%s</cite>', get_comment_author_link() ); ?>
-							</address>
-							<span class="sep">-</span>
-							<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time class="updated published dt-updated dt-published" datetime="<?php comment_time( 'c' ); ?>" itemprop="datePublished dateModified dateCreated">
-							<?php
-							/* translators: 1: date, 2: time */
-							printf( __( '%1$s at %2$s', 'sempress' ), get_comment_date(), get_comment_time() ); ?>
-							</time></a>
-							<?php edit_comment_link( __( '(Edit)', 'sempress' ), ' ' ); ?>
-						</div>
-					</footer>
+				<div class="comment-content p-summary p-name" itemprop="text name description">
+					<?php comment_text(); ?>
 				</div>
+				<footer>
+					<div class="comment-meta commentmetadata">
+						<address class="comment-author p-author author vcard hcard h-card" itemprop="creator" itemscope itemtype="http://schema.org/Person">
+							<?php printf( '<cite class="fn p-name" itemprop="name">%s</cite>', get_comment_author_link() ); ?>
+						</address>
+						<span class="sep">-</span>
+						<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time class="updated published dt-updated dt-published" datetime="<?php comment_time( 'c' ); ?>" itemprop="datePublished dateModified dateCreated">
+						<?php
+						/* translators: 1: date, 2: time */
+						printf( __( '%1$s at %2$s', 'sempress' ), get_comment_date(), get_comment_time() ); ?>
+						</time></a>
+						<?php edit_comment_link( __( '(Edit)', 'sempress' ), ' ' ); ?>
+					</div>
+				</footer>
 			</article>
 		<?php
 				break;
