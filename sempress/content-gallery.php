0d65e46f80d8e8d8625d4845394b5281a020e2fa
Robert van Bregt <robert@robertvanbregt.nl>
2018-08-14 21:06:58 +0200

    Replace inline header with function call for gallery

--- sempress/content-gallery.php
+++ sempress/content-gallery.php
@@ -10,13 +10,7 @@
 ?>
 
 <article <?php sempress_post_id(); ?> <?php post_class(); ?><?php sempress_semantics( 'post' ); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title p-name" itemprop="name headline"><a href="<?php the_permalink(); ?>" class="u-url url" title="<?php printf( esc_attr__( 'Permalink to %s', 'sempress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark" itemprop="name"><?php the_title(); ?></a></h1>
-
-		<div class="entry-meta">
-			<?php sempress_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+	<?php get_template_part( 'entry', 'header' ); ?>
 
 	<?php if ( is_search() ) : // Only display Excerpts for search pages ?>
 	<div class="entry-summary p-summary" itemprop="description">
